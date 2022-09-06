df1ad79ec87a1fa38f0a0f471aae7b4cf570fac0
newby101 <ishimwekevin45@gmail.com>
2022-09-06 14:29:58 +0200

    unstable release-bugs

--- app/src/main/java/com/example/empty/MainActivity.kt
+++ app/src/main/java/com/example/empty/MainActivity.kt
@@ -2,8 +2,9 @@ package com.example.empty
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.*
-import java.security.PublicKey
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,8 +14,7 @@ class MainActivity : AppCompatActivity() {
         val text = findViewById<TextView>(R.id.operation)
         val text_secondary= findViewById<TextView>(R.id.textView)
         var text_keeper = ""
-        var opA: Int = 0
-        var opB: Int
+        var operand = arrayListOf<Int>()
 
         //getting buttnons
         val btn0 = findViewById<Button>(R.id.button_num0)
@@ -32,64 +32,40 @@ class MainActivity : AppCompatActivity() {
         val btn_sub = findViewById<Button>(R.id.button_num_sub)
         val btn_calculate = findViewById<Button>(R.id.calculate)
         val answer = findViewById<TextView>(R.id.answer)
+        val clearbtn =findViewById<Button>(R.id.del)
         //click listeners numbers
-        btn0.setOnClickListener {
-            text_keeper = text.text.toString() + btn0.text.toString()
+       fun texttoinput( v: String): View.OnClickListener? {
+            text_keeper = text.text.toString() +v
             text.text = text_keeper.toString()
+            return null
 
-        }
-        btn1.setOnClickListener {
-            text_keeper = text.text.toString() + btn1.text.toString()
-            text.text = text_keeper.toString()
+       }
+        btn0.setOnClickListener{texttoinput(btn0.text.toString())}
+        btn1.setOnClickListener{texttoinput(btn1.text.toString())}
+        btn2.setOnClickListener{texttoinput(btn2.text.toString())}
+        btn3.setOnClickListener{texttoinput(btn3.text.toString())}
+        btn4.setOnClickListener{texttoinput(btn4.text.toString())}
+        btn5.setOnClickListener{texttoinput(btn5.text.toString())}
+        btn6.setOnClickListener {texttoinput(btn6.text.toString())}
+        btn7.setOnClickListener {texttoinput(btn7.text.toString())}
+        btn8.setOnClickListener {texttoinput(btn8.text.toString())}
+        btn9.setOnClickListener {texttoinput(btn9.text.toString())}
 
-        }
-        btn2.setOnClickListener {
-            text_keeper = text.text.toString() + btn2.text.toString()
-            text.text = text_keeper.toString()
-
-        }
-        btn3.setOnClickListener {
-            text_keeper = text.text.toString() + btn3.text.toString()
-            text.text = text_keeper.toString()
-
-        }
-        btn4.setOnClickListener {
-            text_keeper = text.text.toString() + btn4.text.toString()
-            text.text = text_keeper.toString()
-
-        }
-        btn5.setOnClickListener {
-            text_keeper = text.text.toString() + btn5.text.toString()
-            text.text = text_keeper.toString()
-
-        }
-        btn6.setOnClickListener {
-            text_keeper = text.text.toString() + btn6.text.toString()
-            text.text = text_keeper.toString()
-
-        }
-        btn7.setOnClickListener {
-            text_keeper = text.text.toString() + btn7.text.toString()
-            text.text = text_keeper.toString()
-
-        }
-        btn8.setOnClickListener {
-            text_keeper = text.text.toString() + btn8.text.toString()
-            text.text = text_keeper.toString()
-
-        }
-        btn9.setOnClickListener {
-            text_keeper = text.text.toString() + btn9.text.toString()
-            text.text = text_keeper.toString()
-
-        }
         //operATIONS
+
         btn_add.setOnClickListener {
+
             if (text.text.toString() != "") {
-                opA = text.text.toString().toInt()
-                text_keeper = text.text.toString() + btn_add.text.toString()
-                text_secondary.text = text_keeper.toString()
-                text.text = ""
+
+                operand.add(text.text.toString().toInt())
+                text_keeper=""
+                for(i in operand.indices){
+                    text_keeper +=operand[i].toString()+"+"
+
+                }
+                text_secondary.text = text_keeper
+                text.text=""
+
             }
             else{
             Toast.makeText(this, "empty", Toast.LENGTH_SHORT).show()
@@ -97,10 +73,16 @@ class MainActivity : AppCompatActivity() {
         }
         btn_sub.setOnClickListener {
             if (text.text.toString() != "") {
-                opA = text.text.toString().toInt()
-                text_keeper = text.text.toString() + btn_sub.text.toString()
-                text_secondary.text = text_keeper.toString()
-                text.text = ""
+
+                operand.add(-1*(text.text.toString().toInt()))
+                text_keeper=""
+                for(i in operand.indices){
+                    text_keeper +=operand[i].toString()
+
+                }
+                text_secondary.text = text_keeper
+                text.text=""
+
             }
             else{
                 Toast.makeText(this, "empty", Toast.LENGTH_SHORT).show()
@@ -109,28 +91,32 @@ class MainActivity : AppCompatActivity() {
         }
         btn_calculate.setOnClickListener {
 
-            if (text.text.toString() != "") {
-                opB = text.text.toString().toInt()
-                text_secondary.text=text_secondary.text.toString()+opB.toString()
-                if(text_secondary.text.toString().contains("+")){
-                    val answer_value = opB + opA
-                    answer.text = "=" + answer_value.toString()
-                    text.text = ""
-                }
-                else{
-                    val answer_value = opA - opB
-                    answer.text = "=" + answer_value.toString()
-                    text.text = ""
-                }
+            var answervalue:Int=0
 
+            if (text_secondary.text.toString() != "") {
 
-            } else {
+              for(i in operand.indices){
+                  answervalue=answervalue+operand[i]
+              }
+                answer.text=answervalue.toString()
+            }
+
+            else {
                 Toast.makeText(this, "empty", Toast.LENGTH_SHORT).show()
             }
         }
 
-
+        clearbtn.setOnClickListener{
+            text_secondary.text=""
+            text.text=""
+            answer.text=""
+            operand.clear()
+            Toast.makeText(this, "everything was cleared", Toast.LENGTH_LONG).show()
+        }
     }
 
 
+
+
 }
+
